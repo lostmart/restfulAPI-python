a73dc1ad17cfbb935ec94b6e1987a956f96b0573
lostmart <lostmart@hotmail.com>
2023-09-24 19:52:04 +0200

    new routes

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print('hello')
+from flask import Flask
+
+app = Flask(__name__)
+
+
+# home endpoint ('/')
+@app.route("/")
+def index():
+    return "Hello world"
+
+
+@app.route("/<name>")
+def print_name(name):
+    return f"hi,  {name} !"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
